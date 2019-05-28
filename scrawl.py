68a32d6d2c2425ad958f34c855f915696b7a362a
weiqixu <weiqixuzj@163.com>
2019-05-28 11:21:53 +0800

    更新

--- scrawl.py
+++ scrawl.py
@@ -12,7 +12,7 @@ if __name__=="__main__":
     regionlist = settings.REGIONLIST # only pinyin support
     model.database_init()
     core.GetHouseByRegionlist(regionlist)
-    core.GetRentByRegionlist(regionlist)
+    #core.GetRentByRegionlist(regionlist)
     #core.GetCommunityByRegionlist(regionlist) # Init,scrapy celllist and insert database; could run only 1st time
     communitylist = get_communitylist() # Read celllist from database
     core.GetSellByCommunitylist(communitylist)
