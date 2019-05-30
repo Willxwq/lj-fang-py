0b3542740e9e22d1a7b2a866c39841554b1dbbdd
weiqixu <weiqixuzj@163.com>
2019-05-30 11:31:21 +0800

    测试

--- core.py
+++ core.py
@@ -232,13 +232,17 @@ def get_sell_percommunity(communityname):
                     aa = name.find("span", {"class":"dealCycleTxt"})
 
                     aaa = aa.findAll("span")
-                    logging.info(aaa)
-                    logging.info(aaa[0].get_text().strip()[1:-1])
-                    logging.info(aaa[1].get_text().strip()[3:-1])
 
-                    communityinfo = get_sellInfo_by_url(housetitle.a.get('href'))
-                    for key, value in communityinfo.items():
-                        info_dict.update({key:value})
+                    info_dict.update({u'listing_price' : aaa[0].get_text().strip()[2:-1]})
+                    info_dict.update({u'cycle' : aaa[1].get_text().strip()[4:-1]})
+                    info_dict.update({u'browse_num' : ''})
+                    info_dict.update({u'attention_num' : ''})
+                    info_dict.update({u'view_num' : ''})
+                    info_dict.update({u'adjust_num' : ''})
+
+                    #communityinfo = get_sellInfo_by_url(housetitle.a.get('href'))
+                    #for key, value in communityinfo.items():
+                        #info_dict.update({key:value})
 
                 except:
                     continue
