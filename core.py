41841a4dd31bcdc017b8a5e6bc6d71f39a44c571
weiqixu <weiqixuzj@163.com>
2019-06-11 11:54:44 +0800

    日志配置

--- core.py
+++ core.py
@@ -816,7 +816,7 @@ def log_progress(function, address, page, total):
     logging.info("Progress: %s %s: current page %d total pages %d" %(function, address, page, total))
 
 def convertToUX(content):
-    logging.error(content.encode("unicode_escape"))
+    logging.error(content.encode("utf-8"))
     return content.decode().encode("unicode_escape")
 
     tmp = ""
