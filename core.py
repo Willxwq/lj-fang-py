62fc1b839ee60886afe430626970feaa2a42d5da
weiqixu <weiqixuzj@163.com>
2020-10-21 19:25:17 +0800

    更新

--- core.py
+++ core.py
@@ -112,10 +112,7 @@ def GetRentByRegionlist(regionlist=[u'xicheng']):
 
 def get_house_percommunity(communityname):
     url = BASE_URL + u"ershoufang/rs" + urllib.request.quote(communityname.encode('utf8')) + "/"
-    logging.error(url)
-    exit(0)
     source_code = misc.get_source_code(url)
-    logging.error(source_code)
     soup = BeautifulSoup(source_code, 'lxml')
     if check_block(soup):
         return
